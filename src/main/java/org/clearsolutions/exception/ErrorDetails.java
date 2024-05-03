9d6e510cb083ddc934b4a4328e8dac815b6a836e
Polina <polyapolya404@gmail.com>
2024-05-03 17:58:16 +0300

    fixed

--- src/main/java/org/clearsolutions/exception/ErrorDetails.java
+++ src/main/java/org/clearsolutions/exception/ErrorDetails.java
@@ -1,10 +1,7 @@
 package org.clearsolutions.exception;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
 public record ErrorDetails(LocalDateTime timestamp, String message, String details) {
 
 }
